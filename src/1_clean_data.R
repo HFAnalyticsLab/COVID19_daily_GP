faa8a6be5b6cd1ec30eb6e6c73104682f082893c
Emma Vestesson <emma.vestesson@gmail.com>
2020-04-29 12:41:40 +0200

    Use isoweek instead of week

--- src/1_clean_data.R
+++ src/1_clean_data.R
@@ -13,21 +13,15 @@ ccg <- map(lof, read_csv)
 df <- bind_rows(ccg, .id = 'file') %>% 
   clean_names() %>% 
   mutate(appointment_date=dmy(appointment_date),
-  appt_week= week(appointment_date), 
+  appt_week= isoweek(appointment_date), 
   appt_month= month(appointment_date), 
   appt_year= year(appointment_date), 
   appt_dow=weekdays(appointment_date),
   appt_year_month=ymd(paste0(appt_year,'-', appt_month, '-01')))
- 
+
 saveRDS(df, here::here('data', 'CCG_GP.rds'))
 
-national <- df %>% 
-  group_by(appt_year_month, appt_status) %>% 
-  summarise(appt_status_count=n())
-
-
-ggplot(national, aes(x=appt_week, y=count)) + geom_col() + facet_wrap(~appt_year)
-ggplot(national, aes(x=appt_year_month, y=appt_status_count, 
-                     group=appt_status, fill= appt_status)) + 
-  geom_col(position = "fill") + theme_THF() + scale_fill_THF()
-
+coverage_raw <- read_csv(here::here('data', 'original data', 'APPOINTMENTS_GP_COVERAGE.csv')) %>% 
+  clean_names() %>% 
+  mutate(appointment_month=dmy(appointment_month)) 
+saveRDS(coverage_raw, here::here('data', 'GP_coverage.rds'))
